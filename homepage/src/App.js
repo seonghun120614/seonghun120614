d34c149133d69bd9d5e6d10ea3871016e6054fd6
seonghun120614 <seonghun120614@gmail.com>
2022-03-19 15:15:23 +0000

    end of creating web-frame

---
+++ homepage/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p style={{
+        fontStyle : "bold",
+      }}>Start <code>Front End</code></p>
+    </div>
+  );
+}
+
+export default App;
